b1eeb199801b56b52fde93dcbd3b800d4c729a08
Pierre Poulain <pierre.poulain@cupnet.net>
2023-02-09 00:58:59 +0100

    Create Bash script to build results

---
+++ make_results.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/env bash
+
+rm -rf results/*
+jupyter nbconvert --to html  --execute --allow-errors --output-dir results notebooks/paper_numbers_figures.ipynb
+jupyter nbconvert --to html  --execute --allow-errors --output-dir results notebooks/search_MD_in_pubmed.ipynb
+mkdir -p results/fig
+cp notebooks/fig/*.{svg,png} results/fig/
+cp notebooks/*.tsv results/
