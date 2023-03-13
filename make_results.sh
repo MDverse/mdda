e212e8c26d84079b62691180c483650d754f49e9
Pierre Poulain <pierre.poulain@cupnet.net>
2023-03-13 23:15:04 +0100

    Update results in notebooks

--- make_results.sh
+++ make_results.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 
 rm -rf results/*
-jupyter nbconvert --to html  --execute --allow-errors --output-dir results notebooks/paper_numbers_figures.ipynb
+jupyter nbconvert --to html  --execute --allow-errors --output-dir results notebooks/paper_stats_and_figures.ipynb
 jupyter nbconvert --to html  --execute --allow-errors --output-dir results notebooks/search_MD_in_pubmed.ipynb
 mkdir -p results/fig
 cp notebooks/fig/*.{svg,png} results/fig/
